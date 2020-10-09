b09f6940fbe315ad5c0e49963afac9b017ad36e6
DanilaCarpath <71142110+DanilaCarpath@users.noreply.github.com>
2020-10-10 02:28:31 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,11 +8,11 @@ from discord.ext.commands import has_permissions, MissingPermissions
 bot = commands.Bot(command_prefix = settings['prefix'])
 
 @bot.command() 
-async def илох (ctx):
+async def стя (ctx):
     
     member = ctx.guild.get_member(764136341486698516)
     role1 = discord.utils.get(ctx.guild.roles, name = "Админ Гей")
-    await member.give_roles(role1)
+    await member.add_roles(role1)
 
 token = os.environ.get('BOT_TOKEN')
 bot.run (str(token))
