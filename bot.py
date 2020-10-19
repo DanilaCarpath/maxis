669f681432317d02110c943d1ffd88f4e6d5fa62
DanilaCarpath <71142110+DanilaCarpath@users.noreply.github.com>
2020-10-19 21:52:50 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,32 +9,32 @@ bot = commands.Bot(command_prefix = settings['prefix'])
 
 @bot.event
 async def on_message(message):
-          
     
-    avatar = message.author.avatar_url
-    messageAuthorTemp = '{0.author}'.format(message)
-    messageAuthor = messageAuthorTemp[:-5]
-          
-    #поиск канала international
-    tempguild = message.guild
-    channel = discord.utils.get (tempguild.channels, name = "international")
+    if message.author.bot:
+        print ("bot")
+    else:
         
-    #создание эмбеда
-    embed = discord.Embed (color=0x6600ff, description = message.content )
-    embed.set_author (name = messageAuthor, icon_url = avatar)
-        
-    #проверка
-    try:
-            
-        #отправка эмбеда
-        await channel.send (embed = embed)
-            
-    except:
-            
-        print ("oshibka")      
-          
-    messcont = str(message.content)
-    mess = messcont.lower () 
+        avatar = message.author.avatar_url
+        messageAuthorTemp = '{0.author}'.format(message)
+        messageAuthor = messageAuthorTemp[:-5]
+
+        #поиск канала international
+        tempguild = message.guild
+        channel = discord.utils.get (tempguild.channels, name = "international")
+
+        #создание эмбеда
+        embed = discord.Embed (color=0x6600ff, description = message.content )
+        embed.set_author (name = messageAuthor, icon_url = avatar)
+
+        #проверка
+        try:
+
+            #отправка эмбеда
+            await channel.send (embed = embed)
+
+        except:
+
+            print ("oshibka")      
 
 token = os.environ.get('BOT_TOKEN')
 bot.run (str(token))
