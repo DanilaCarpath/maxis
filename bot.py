e2f714c10b289fc218bb582bbe6050d99e805acc
DanilaCarpath <71142110+DanilaCarpath@users.noreply.github.com>
2020-10-20 02:22:55 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,8 +12,10 @@ bot = commands.Bot(command_prefix = settings['prefix'])
 @bot.command()
 async def dm (ctx):
     for i in ctx.guild.members:
-        await i.send ("Внимание! Кекляндия переезжает в Конфу, новую площадку для РП. Для Кекляндии будет отдельный рп уголок, а во время отдыха от рп вы можете поговорить в чате. https://discord.gg/BZ7NQ2m")
-
+        try:
+            await i.send ("Внимание! Кекляндия переезжает в Конфу, новую площадку для РП. Для Кекляндии будет отдельный рп уголок, а во время отдыха от рп вы можете поговорить в чате. https://discord.gg/BZ7NQ2m")
+        except:
+            print ("oshibka")
         
 token = os.environ.get('BOT_TOKEN')
 bot.run (str(token))
